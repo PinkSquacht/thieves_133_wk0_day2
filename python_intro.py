8dc133269c985bee1d05f8c5f44fec668184443c
PinkSquacht <Lloyd.bowar@gmail.com>
2023-10-04 21:07:07 -0700

    this is my notes through the lecture

---
+++ python_intro.py
@@ -0,0 +1,132 @@
+# comments! this is a comment
+print("Hello Thieves!")
+# fueanfuw
+# fnefnau
+# use ctrl-/ to comment of line 
+
+# varibles
+x = 5
+number3 = 58271
+
+# Data Types
+# intergers AND floats
+num1 = 8
+print(num1)
+print(type(num1))
+
+num2 = 8.0
+print(num2)
+print(type(num2))
+
+num3 = 5.99823
+print(int(num3))
+
+
+# math operations
+print("Math"\n)
+# add
+add = 5 + 5
+print(add)
+# subraaction
+sub = 10 - 5
+print(sub)
+# multiplecation
+sum = 5 * 5
+print(sum) 
+# divsion 
+div = 5 / 5
+# expontant
+powerful = 5 ** 3
+print(powerful)
+
+# floor division //
+fdiv = 26 // 5
+print(fdiv)
+# modulo
+mod = 26 % 5
+print(mod)
+
+print(24 % 2 == 0) #evens
+print(25 % 2 != 0) #odds 
+
+# strings
+print("Strings")
+# they're ordered, immutealbe, itreable
+# you can use single or double qoutes; either is fine BUT watch the interactions
+st1 = 'single qoute string'
+st2 = "double qoutes"
+st3 = "oops we have a problem"
+st4 = 'or did we "really"'
+st5 = 'oh no we\'ve messed this up'
+print(st5)
+
+print(st1[0])
+print(len(st3))
+
+# manipulation strings
+
+# concatenation + interpolation 
+print(st1 + st4) # simple
+print(st2 +' : : ' + st3) # adding a letreal
+print(st2 + str(mod) + ' ' + '<that was funky>')
+
+# watch recoding to finish this section of notes
+# f_str notes needed
+
+# string methods
+print(st5.upper())
+print(st5)
+
+
+# incrementing and decrementing
+num5 = 234
+num6 = 987
+print(num5 + num6)
+print(num5, num6)
+num5 =  num5 + 6
+print(num5)
+
+
+# list 
+a_list[]
+print(a_list)
+nums_list[10, 20, 30, 40, 50]
+print(nums_list, type(nums_list), len(nums_list), nums_list[0], nums_list[-1])
+print(nums_lists)
+rando_list = [1, "2", 3.0, True, None, []]
+print(rando_list)
+
+# list methods 
+# append() 
+# adds to the end of the list 
+a_list.append(5)
+print(a_list)
+
+# .pop()
+# removes an item from the list by postions, defaults to the last. returns the value so you could to save it a varible
+a_list.pop()
+print(a_list)
+a_list.pop(0)
+print(a_list)
+
+# .remove()
+# remove takes out or removes the FIRST occurence of a VALUE
+a_list.append(50)
+a_list.append(75)
+a_list.append(50)
+print(a_list)
+a_list.append(50)
+a_list.remove(50)
+print(a_list)
+
+
+
+
+
+
+
+# funtions verus methods
+
+
+
+#  'protected' words/terms --> be careful with varible names
